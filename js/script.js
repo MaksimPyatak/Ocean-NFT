fc30c89c5d06be20da7647a17d5aa3a634e5b882
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2022-08-16 12:41:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,14 +23,17 @@ const swiper = new Swiper('.top-nft__swiper', {
  let tab;
  let tabContent;
  
+ //Для section top-selers. Зміна колоьору
  const valueGrowth = document.getElementsByClassName('seler-card__growth');
-
+ 
+ 
+ //Для T A B 's
  window.onload = function tabS() {
    tab = document.getElementsByClassName('colection-section__tab');
    tabContent = document.getElementsByClassName('colection-section__example');
    hideTabContents(1);
 
-   //For section top-selers. Зміна колоьору
+   //Для section top-selers. Зміна колоьору
    for (let i = 0; i < valueGrowth.length; i++) {
       let x = valueGrowth[i].innerHTML;
       let y = x.slice(0,-1);
@@ -52,7 +55,6 @@ let tabs = document.querySelector('.colection-section__tabs');
 let tabs1 = document.getElementById('colection-section__tabs1');
 tabs.onclick = function (event) {
    let target = event.target;
-   console.log(target);
    if (target.className == 'colection-section__tab') {
       for (let i = 0; i < tab.length; i++) {    
          if (target == tab[i]) {
@@ -78,5 +80,77 @@ const swiperArtWorkers = new Swiper('.art-workers__swiper', {
    centeredSlides: true,
  });
 
-// ------------COLOR SECTION--------------
+// ------------Для footer. Випадаючий список--------------
 
+//При зміні розширення екрана потріно перезавантаження сторінки. 
+//Тож швидше за все при повертанні телефону буде працювати не коректно.
+//Краще зробити все через класи або продумати іншу логіку.
+//Вирішив проблему через медіазапит
+
+//Для footer. Випадаючий список
+const openList = document.getElementsByClassName('footer__item__open');
+const openListLeft = document.getElementsByClassName('footer__opener-left');
+const openListRight = document.getElementsByClassName('footer__opener-right');
+const openListTitle = document.getElementsByClassName('footer__title');
+const footerList = document.getElementsByClassName('footer__list');
+const footerOpenerRight = document.getElementsByClassName('footer__opener-right');
+const footerOpenerLeft = document.getElementsByClassName('footer__opener-left');
+
+
+let dropdown = document.querySelector('.footer__dropdown'); 
+//let tabs1 = document.getElementById('colection-section__tabs1');
+dropdown.onclick = function (e) {
+   if (window.matchMedia("(max-width: 1024px)").matches) {
+      let target = e.target;
+      if (target.className == 'footer__title' || 'footer__item__open') {
+         for (let i = 0; i < openList.length; i++) {    
+            if (target == openListTitle[i]) {
+               showFooterList(i);
+               break;
+            }  else if (target == openList[i]) {
+               showFooterList(i);
+               break;
+            } else if (target == openListLeft[i]) {
+               showFooterList(i);
+               break;
+            }  else if (target == openListRight[i]) {
+               showFooterList(i);
+               break;
+            }               
+         }
+      }   
+   }
+};
+function showFooterList(open) {
+   let styleFooterList = window.getComputedStyle(footerList[open]);
+   let display = styleFooterList.getPropertyValue('display');
+   if (display == "none") {
+      for (let i = 0; i < footerList.length; i++) { 
+         footerList[i].style.display = "none";
+         footerOpenerRight[i].classList.remove('_open');
+         footerOpenerLeft[i].classList.remove('_open');
+      };
+      footerList[open].style.display = "block";
+      footerOpenerRight[open].classList.add('_open');
+      footerOpenerLeft[open].classList.add('_open');
+   } else {
+      footerList[open].style.display = "none";
+      footerOpenerRight[open].classList.remove('_open');
+      footerOpenerLeft[open].classList.remove('_open');
+   }   
+}
+const mql = window.matchMedia('(min-width: 1025px)');
+function screenTest(e) {
+   if (e.matches) {
+      for (let i = 0; i < footerList.length; i++) { 
+         footerList[i].style.display = "block";
+      };
+   }
+      else {
+         for (let i = 0; i < footerList.length; i++) { 
+            footerList[i].style.display = "none";
+         };
+      }
+}
+
+mql.addEventListener('change', screenTest);
